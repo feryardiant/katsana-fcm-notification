1ed3e5f5a40712d92ca080e98c031bb738073206
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-12-04 11:15:58 +0700

    Init fcm
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ src/Jobs/PersonalAccidentPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NotificationChannels\Fcm\Jobs;
+
+use Illuminate\Queue\SerializesModels;
+
+class SendPersonalNotification
+{
+    use SerializesModels;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ src/Notifications/TripSummary.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NotificationChannels\Fcm\Notifications;
+
+use NotificationChannels\Fcm\Message;
+
+class TripSummary extends Notification
+{
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \NotificationChannels\Fcm\Message
+     */
+    public function toFcm($notifiable)
+    {
+        $data = [
+            'pushType' => 'sync',
+            'key' => 'trip-summary',
+            'distance' => $notifiable->summary->distance,
+            'duration' => $notifiable->summary->distance,
+            'start_time' => $notifiable->summary->distance,
+            'start_latitude' => $notifiable->summary->distance,
+            'start_longitude' => $notifiable->summary->distance,
+            'end_time' => $notifiable->summary->distance,
+            'end_latitude' => $notifiable->summary->distance,
+            'end_longitude' => $notifiable->summary->distance,
+            'vehicle_id' => $notifiable->summary->vehicleId,
+        ];
+
+        return (new Message)
+                    ->topic('notify-user-trip-summary')
+                    ->data($data);
+    }
+}
